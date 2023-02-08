c35c9fabab3996bf5506361639c1069fef78837d
DereckAn <dereckangeles15071998@gmail.com>
2023-02-08 15:43:29 -0700

    rememberme drawer

--- lib/login.dart
+++ lib/login.dart
@@ -5,6 +5,7 @@ import 'package:crypto/crypto.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:streamer/Home.dart';
 import 'package:streamer/helpers/helpers.dart';
 import 'package:streamer/subsonic/context.dart';
@@ -27,10 +28,31 @@ class _Login extends State<Login> {
   String _server = "";
   String _username = "";
   String _password = "";
+  SharedPreferences? _prefs;
+  bool isRemeberMe = false;
+
+ 
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.menu),
+        onPressed: () {
+          scaffoldKey.currentState?.openEndDrawer();
+        },
+        backgroundColor: Colors.transparent,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      endDrawer: Drawer(backgroundColor: Color.fromARGB(100, 0, 0, 0),
+        child: ListView(children: [
+          DrawerHeader(
+              child: Image.network(
+                  "https://avatars.githubusercontent.com/u/108163041?s=96&v=4"))
+        ]),
+      ),
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
         child: GestureDetector(
@@ -48,7 +70,6 @@ class _Login extends State<Login> {
                   radius: .8,
                 )),
                 child: SingleChildScrollView(
-                  // physics: AlwaysScrollableScrollPhysics(),
                   padding:
                       const EdgeInsets.symmetric(horizontal: 25, vertical: 120),
                   child: Column(
@@ -62,6 +83,8 @@ class _Login extends State<Login> {
                             fontWeight: FontWeight.bold),
                       ),
                       const SizedBox(height: 130),
+                      rememberMe(),
+                      const SizedBox(height: 15),
                       server(),
                       const SizedBox(height: 20),
                       name(),
@@ -82,6 +105,42 @@ class _Login extends State<Login> {
       ),
     );
   }
+
+  Widget rememberMe() {
+    return Container(
+      height: 20,
+      child: Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+        Theme(
+          data: ThemeData(
+            unselectedWidgetColor: Color.fromARGB(115, 0, 0, 0),
+          ),
+          child: Checkbox(
+            value: isRemeberMe,
+            checkColor: Colors.red,
+            activeColor: Color.fromARGB(115, 0, 0, 0),
+            onChanged: (value) {
+              setState(() {
+                isRemeberMe = value!;
+                _prefs!.setBool("check", value);
+                if (isRemeberMe) {
+                  _prefs!.setString("_name", _name);
+                  _prefs!.setString("_server", _server);
+                  _prefs!.setString("_username", _username);
+                  _prefs!.setString("_password", _password);
+                }
+              });
+            },
+          ),
+        ),
+        Text(
+          "Remember Me",
+          style: TextStyle(
+              color: Color.fromARGB(115, 0, 0, 0), fontWeight: FontWeight.bold),
+        ),
+      ]),
+    );
+  }
+
 
   Widget server() {
     return Column(
