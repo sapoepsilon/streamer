3fc28f83ae2b8eb9ab9308efbd79b64ac01e8309
sapoepsilon <sapoepsilon@gmail.com>
2023-02-09 22:27:10 +0000

    should be working now

--- lib/home.dart
+++ lib/home.dart
@@ -43,7 +43,7 @@ class _HomeState extends State<Home> {
           ),
         ),
       ),
-      body: Column( // check if linter is working
+      body: Column( // check if linter is working should work now
         children: const [
           SizedBox(
             height: 30,
