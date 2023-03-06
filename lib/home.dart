7d7804b4f286c9a97e3543318d45cbc93ff57995
izzy <sapoepsilon@gmail.com>
2023-03-06 16:28:15 -0700

    navigate to songs from howe, fix code style

--- lib/home.dart
+++ lib/home.dart
@@ -3,11 +3,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:streamer/pages/Songs_List.dart';
 import 'package:streamer/player.dart';
 import 'package:streamer/search.dart';
-
-
-
 
 class Home extends StatefulWidget {
   const Home({super.key, required this.subSonicContext});
@@ -15,24 +13,19 @@ class Home extends StatefulWidget {
   final subSonicContext;
 
   @override
-  State<Home> createState() => _home();
+  State<Home> createState() => _HomeState();
 }
 
-class _home extends State<Home> {
-  
-   @override
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       extendBody: true,
       bottomNavigationBar: _buildBottomNavigationBar(),
-      
       backgroundColor: Colors.transparent,
-      body: 
-           
-      Container(
+      body: Container(
         height: double.infinity,
-                width: double.infinity,
+        width: double.infinity,
         decoration: const BoxDecoration(
           gradient: RadialGradient(
             colors: [
@@ -42,87 +35,77 @@ class _home extends State<Home> {
             radius: 0.8,
           ),
         ),
-        child: 
-        Column(
-                  children: <Widget>[
-                    
-                    Container(
-                      alignment: Alignment.centerLeft,
-                       child: Transform.translate(
-                         // ignore: prefer_const_constructors
-                         offset: Offset(0, 150),
-                           // ignore: prefer_const_constructors
-                           child: Text(
-                             "Hello " ,
-                     
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    ),
-                    ),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                       child: Transform.translate(
-                         // ignore: prefer_const_constructors
-                         offset: Offset(0, 155),
-                           // ignore: prefer_const_constructors
-                           child: Text(
-                             "What do you want to listen today?",
-                     
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.normal),
-                    ),
-                    ),
-                    ),
-                    //Search bar here(Delete after actually having one)
-                    Container(
-                      alignment: Alignment.center,
-                       child: Transform.translate(
-                         // ignore: prefer_const_constructors
-                         offset: Offset(0, 220),
-                           // ignore: prefer_const_constructors
-                           child: Text(
-                             "Search bar here " ,
-                     
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    ),
-                    ),
-                   //Album Slide here(Delete after actually having one)
-                    Container(
-                      alignment: Alignment.center,
-                       child: Transform.translate(
-                         // ignore: prefer_const_constructors
-                         offset: Offset(0, 260),
-                           // ignore: prefer_const_constructors
-                           child: Text(
-                             "Album slide" ,
-                     
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    ),
-                    ),
-                    // buildforgotPassBtn(),
-                  ],
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Transform.translate(
+                // ignore: prefer_const_constructors
+                offset: Offset(0, 150),
+                // ignore: prefer_const_constructors
+                child: Text(
+                  "Hello ",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
                 ),
-        
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Transform.translate(
+                // ignore: prefer_const_constructors
+                offset: Offset(0, 155),
+                // ignore: prefer_const_constructors
+                child: Text(
+                  "What do you want to listen today?",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal),
                 ),
-
-                
-                
+              ),
+            ),
+            //Search bar here(Delete after actually having one)
+            Container(
+              alignment: Alignment.center,
+              child: Transform.translate(
+                // ignore: prefer_const_constructors
+                offset: Offset(0, 220),
+                // ignore: prefer_const_constructors
+                child: Text(
+                  "Search bar here ",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            //Album Slide here(Delete after actually having one)
+            Container(
+              alignment: Alignment.center,
+              child: Transform.translate(
+                // ignore: prefer_const_constructors
+                offset: Offset(0, 260),
+                // ignore: prefer_const_constructors
+                child: Text(
+                  "Album slide",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            // buildforgotPassBtn(),
+          ],
+        ),
+      ),
     );
-  
   }
+
   BottomAppBar _buildBottomNavigationBar() {
     return BottomAppBar(
       color: const Color.fromRGBO(0, 0, 0, 0.5),
@@ -148,7 +131,12 @@ class _home extends State<Home> {
             color: Colors.white,
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(platformPageRoute(
+                  builder: (context) => SongsList(subSonicContext: widget.subSonicContext),
+                  // ignore: todo
+                  context: context));
+            },
             icon: Icon(
               isCupertino(context) ? CupertinoIcons.heart : Icons.hearing_sharp,
               color: Colors.white,
@@ -169,8 +157,4 @@ class _home extends State<Home> {
       ),
     );
   }
-
- 
 }
-
-
