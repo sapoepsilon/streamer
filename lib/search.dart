2647124e3966f8a9f692b15c07886d8bb8d39783
Justin Schow <justin.schow@gmail.com>
2023-03-06 09:07:19 -0700

    Update search.dart

--- lib/search.dart
+++ lib/search.dart
@@ -16,75 +16,105 @@ class _Search extends State<Search> {
         // title: Text(widget.title),
         actions: [
           IconButton(
+            icon: const Icon(Icons.search),
             onPressed: () {
               showSearch(
                 context: context,
-                delegate: CustomSearchDelegate(),
+                delegate: CustomSearch(),
               );
             },
-            icon: const Icon(Icons.search),
           ),
         ],
       ),
+      body: Center(
+          child: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [Colors.purple, Colors.teal])))),
     );
   }
 }
 
-class CustomSearchDelegate extends SearchDelegate {
-  List<String> searchTerms = ['Quesariya', 'Lofi music', 'Believer'];
+class CustomSearch extends SearchDelegate {
+  List<String> playlist = [
+    'The End of the Line - The Traveling Wilburys',
+    'Fight for Your Right - Beastie Boys',
+    'B is for Brutus - The Hives',
+    'Work Hard, Play Hard - Wiz Khalifa',
+    'Africa - Toto',
+    'Young Blood - The Naked and Famous',
+    'Time for Me to Fly - REO Speedwagon',
+    'Howlin for You - The Black Keys',
+    'Live and Let Die - Guns N Roses',
+    'Radioactive - Imagine Dragons',
+    'One Foot - WALK THE MOON',
+    'The High Road - Broken Bells',
+    'The Rent I Pay - Spoon',
+    'Lightning Crashes - Live',
+    'Lake Michigan - Rogue Wave',
+    'Choker - twenty one pilots',
+    'Big Sur - Jack Johnson',
+    'High School Never Ends - Bowling for Soup',
+    'Apologize - grandson',
+    'Wake Me Up - Avicii'
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
       IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+          })
     ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
     return IconButton(
-      icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    List<String> matchquery = [];
-    for (var title in searchTerms) {
-      if (title.toLowerCase().contains(query.toLowerCase())) {
-        matchquery.add(title);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchquery.length,
-      itemBuilder: (context, index) {
-        var result = matchquery[index];
-        return ListTile(title: Text(result));
-      },
-    );
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () {
+          close(context, null);
+        });
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    List<String> matchquery = [];
-    for (var title in searchTerms) {
-      if (title.toLowerCase().contains(query.toLowerCase())) {
-        matchquery.add(title);
+    List<String> matchQuery = [];
+    for (var item in playlist) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
       }
     }
     return ListView.builder(
-      itemCount: matchquery.length,
-      itemBuilder: (context, index) {
-        var result = matchquery[index];
-        return ListTile(title: Text(result));
-      },
-    );
+        itemCount: matchQuery.length,
+        itemBuilder: (context, index) {
+          var result = matchQuery[index];
+          return ListTile(
+            title: Text(result),
+          );
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in playlist) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+        itemCount: matchQuery.length,
+        itemBuilder: (context, index) {
+          var result = matchQuery[index];
+          return ListTile(
+            title: Text(result),
+            tileColor: Colors.teal,
+          );
+        });
   }
 }
